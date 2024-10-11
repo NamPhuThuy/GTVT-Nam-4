25d821d61cfd5d4752042b08d350946f8a9d45f6
NamPhuThuy <ttnam957@gmail.com>
2024-10-12 06:50:20 +0700

    Update

--- HocKi7/XuLyAnh/Bài tập nhóm - tìm hiểu về lọc tần số/DFT_base.py
+++ HocKi7/XuLyAnh/Bài tập nhóm - tìm hiểu về lọc tần số/DFT_base.py
@@ -19,13 +19,14 @@ def DFT1D(img):
 
 def IDFT1D(img):
     U = len(img)
-    outarry = np.zeros(U, dtype=complex)
+    outarry = np.zeros(U, dtype = complex)
     for n in range(U):
         sum = 0.0
         for m in range(U):
             e = np.exp(1j * 2 * np.pi * m * n / U)
-            sum += img[m]*e
-            print(f"accumulated sum={sum}")
-        pixel = sum/U
-        outarry[n]=pixel
+            sum += img[m] * e
+            print(f"accumulated sum = {sum}")
+        pixel = sum / U
+        outarry[n] = pixel
+        print(f"DFT[{m}] = {outarry[m]}")
     return outarry
